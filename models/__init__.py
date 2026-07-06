4c7c4620da90e207767aad89084c718d01cd8b18
daeyu666 <384114681@qq.com>
2026-07-06 17:57:58 +0800

    export stage-2 reliability modules

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,17 @@
 """RAPD-Net model definitions."""
 
 from .stage1_unmixing import Stage1UnmixingNet
+from .stage2_frequency_reliability import (
+    ChannelWiseSpectralSplitter,
+    FrequencyReliabilityScreen,
+    NoiseSplitter,
+    SharedMSIFeatureEncoder,
+)
 
-__all__ = ["Stage1UnmixingNet"]
+__all__ = [
+    "Stage1UnmixingNet",
+    "SharedMSIFeatureEncoder",
+    "ChannelWiseSpectralSplitter",
+    "NoiseSplitter",
+    "FrequencyReliabilityScreen",
+]
