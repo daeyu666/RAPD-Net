96a02f8525b40de7d5e99ab2dfeddd380f244e7e
daeyu666 <384114681@qq.com>
2026-07-07 15:55:59 +0800

    export dual-space stage-2 model

--- models/__init__.py
+++ models/__init__.py
@@ -3,6 +3,7 @@
 from .stage1_spectral_basis import Stage1SpectralBasisNet
 from .stage1_unmixing import Stage1UnmixingNet
 from .stage2_coefficient_residual import Stage2CoefficientResidualNet
+from .stage2_dual_space import Stage2DualSpaceNet
 from .stage2_frequency_reliability import (
     ChannelWiseSpectralSplitter,
     FrequencyReliabilityScreen,
@@ -17,6 +18,7 @@ __all__ = [
     "Stage1UnmixingNet",
     "Stage2CoefficientResidualNet",
     "Stage2SRFAnchorNet",
+    "Stage2DualSpaceNet",
     "SharedMSIFeatureEncoder",
     "ChannelWiseSpectralSplitter",
     "NoiseSplitter",
