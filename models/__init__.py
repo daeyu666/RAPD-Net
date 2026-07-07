2d01518a6574d8b5413815f3f35f98bff9d4aaab
daeyu666 <384114681@qq.com>
2026-07-07 09:33:14 +0800

    export new stage-2 coefficient residual model

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,7 @@
 
 from .stage1_spectral_basis import Stage1SpectralBasisNet
 from .stage1_unmixing import Stage1UnmixingNet
+from .stage2_coefficient_residual import Stage2CoefficientResidualNet
 from .stage2_frequency_reliability import (
     ChannelWiseSpectralSplitter,
     FrequencyReliabilityScreen,
@@ -13,6 +14,7 @@ from .stage2_physical_fusion import Stage2PhysicalFusionNet
 __all__ = [
     "Stage1SpectralBasisNet",
     "Stage1UnmixingNet",
+    "Stage2CoefficientResidualNet",
     "SharedMSIFeatureEncoder",
     "ChannelWiseSpectralSplitter",
     "NoiseSplitter",
